7ae172193b57af5597ffa4e3ad6308234d5ea967
TheYautja <am.96138669@gmail.com>
2026-02-23 23:36:04 -0300

    Update kernel.c
    add compiler checks and VBA test

--- kernel.c
+++ kernel.c
@@ -1,5 +1,18 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+#if defined(__linux__)
+#error "cross compiler not found"
+#endif
+
+
+#if !defined(__i386__)
+#error "needs a x86 compiler"
+#endif
 
 
 void kernel_main(void){
-	
+	*((int*)0xb8000)=0x07690748;
 }
